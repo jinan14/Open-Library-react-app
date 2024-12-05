550037a1be4015bd846d25adc27dae7f9513a715
jinan14 <jinanghanam@gmail.com>
2024-12-05 14:11:43 -0800

    open library react app

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -30,20 +30,20 @@ const Login = () => {
     };
 
     return (
-        <div className="flex items-center w-full justify-between h-screen">
-            <div className='w-[60%]'>
-                <img className='object-cover w-full h-full' src="/book.jpeg" alt="" />
+        <div className="flex items-center justify-between h-screen w-full flex-col md:flex-row">
+            <div className='w-full h-[40%] md:h-full md:w-[60%]'>
+                <img className='object-cover w-full h-full' src="/book.jpeg" alt="Book" />
             </div>
-            <div className='w-[40%] h-full m-auto flex flex-col gap-3 p-6 items-center justify-center'>
-                <div className='w-full flex flex-col gap-3 items-center justify-center'>
+            <div className='w-full md:w-[40%] h-full m-auto flex flex-col gap-3 p-6 items-center justify-center'>
+                <div className='w-[70%] border border-gray-500 rounded-xl p-5 flex flex-col gap-3 items-center justify-center'>
                     <h1 className="text-4xl font-semibold mb-2">Login Page</h1>
-                    <div className='flex flex-col'>
+                    <div className='flex flex-col w-full'>
                         <label htmlFor="username" className='mb-2'>Name</label>
                         <input 
                             id='username' 
                             type="text" 
                             placeholder="Username" 
-                            className="border p-2 mb-6" 
+                            className="border p-2 mb-6 w-full sm:w-[80%] md:w-full" 
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
                         />
@@ -52,12 +52,12 @@ const Login = () => {
                             id='password' 
                             type="password" 
                             placeholder='Password' 
-                            className="border p-2 mb-2" 
+                            className="border p-2 mb-2 w-full sm:w-[80%] md:w-full" 
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
-                    {error && <p className="text-red-500 mb-2">{error}</p>} {/* Display error message */}
+                    {error && <p className="text-red-500 mb-2">{error}</p>}
                     <button onClick={handleLogin} className="bg-slate-500 text-white p-2 w-32 rounded-3xl hover:bg-cyan-800 focus:ring-4">
                         Login
                     </button>
@@ -65,6 +65,7 @@ const Login = () => {
             </div>
         </div>
     );
+    
 };
 
 export default Login;
